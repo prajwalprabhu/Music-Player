a86a78b36e8305edca743e06fee365638a6933a4
prajwalprabhu <prajwalprabhu.tellar@gmail.com>
2020-08-13 17:06:10 +0530

    13/8

--- music_player.py
+++ music_player.py
@@ -9,21 +9,19 @@ from json import load,dump,JSONDecodeError
 from threading import Thread
 from mutagen.mp3 import MP3
 from pyttsx3 import speak
-# from PIL import Image,ImageTk
 class music_player():
     def __init__(self):
         self.play_list=[]
         self.file_dir=[]
         self.file_name_dir={}
         self.udplay_list={}
-        # print(type(self.udplay_list),"udplay_list")
         self.exited=False
         self.name=None
         self.playing=False
         self.paused=False
         self.audio_format=[".mp3",".MP3",".wav",".3gp",".aa",".aax",".avi",".ogg"]
-        # loadd=Image.open(join(getcwd(),"img/play.png"))
-        # self.play_img=ImageTk.PhotoImage(loadd)
+        self.gui()
+    def gui(self):
         self.root=Tk()
         self.speak_=BooleanVar()
         self.root.title("Music Player")
@@ -33,22 +31,6 @@ class music_player():
             self.root.iconbitmap(normcase(join(getcwd(),"img/tkicon.ico")))
         self.root.geometry("250x300")
         self.list_window=Frame(self.root)
-        menu=Menu(self.root)
-        self.root.config(menu=menu)
-        self.root.bind("<Control-O>",self.open_folder)
-        self.root.bind("<Alt-leftarrow>",self.previous)
-        self.root.bind("<Alt-rightarrow>",self.next)
-        self.root.bind("<space>",self.play)
-        File=Menu(menu)
-        File.add_command(label="Add Folder ctrl+shift+o",command=self.open_folder)
-        File.add_command(label="Close Floder",command=self.close_floder)
-        playlist_menu=Menu(menu)
-        playlist_menu.add_command(label="CreatePlaylist",command=self.creat_playlist)
-        self.list_menu=Menu(menu)
-
-        playlist_menu.add_cascade(label="PLay Playlist",menu=self.list_menu)
-        menu.add_cascade(label="File",menu=File)
-        menu.add_cascade(label="Playlist",menu=playlist_menu)
         self.list_window.pack(side=TOP)
         self.title_label=Label(self.root)
         self.title_label.pack()  
@@ -58,41 +40,14 @@ class music_player():
         self.button_window.pack(side=BOTTOM)
         self.list=Listbox(self.list_window,width=30)
         self.list.grid(row=0,column=0)
-        repeat=True
+        self.gui_menu()
+        self.gui_data_init()
+        self.root.bind("<Control-O>",self.open_folder)
+        self.root.bind("<Alt-leftarrow>",self.previous)
+        self.root.bind("<Alt-rightarrow>",self.next)
+        self.root.bind("<space>",self.play)
         self.list.bind("<Button-1>",self.play2)
-        # self.list.bind("<Key-o>",lambda : self.play(repeat=True))
-        self.get_data()
-        if self.volume == None:
-            self.volume=25
-        if self.udplay_list==None or type(self.udplay_list) ==list:
-            self.udplay_list={}
-        def main_playlist():
-            self.list.delete(0,END)
-            
-            self.get_data()
-        self.list_menu.add_command(label="Main",command=main_playlist) 
-        for pllist in self.udplay_list:
-            self.list_menu.add_command(label=pllist,command=lambda :self.play_play_list(pllist))
-        
-
-        
-        self.image4=PhotoImage(file=join(getcwd(),"img/previous.png"))
-        # startfile(join(getcwd(),"img/previous.png"))
-        self.previous_button=Button(self.button_window,command=self.previous,image=self.image4)
-        self.previous_button.grid(row=0,column=1)
-        self.image2=PhotoImage(file=join(getcwd(),"img/play.png"))
-        self.button=Button(self.button_window,command=self.play,image=self.image2)
-        self.button.grid(row=0,column=2)
-        self.volume_scale=Scale(self.button_window,orient='vertical',variable=self.volume,from_=100,to=0)
-        self.volume_scale.grid(row=0,column=5)
-        self.image3=PhotoImage(file=join(getcwd(),"img/next.png"))
-        self.next_button=Button(self.button_window,command=self.next,image=self.image3)
-        self.next_button.grid(row=0,column=3)
-        self.check_button=Checkbutton(self.button_window,text="Say name",variable=self.speak_,onvalue=True,offvalue=False)
-        self.check_button.deselect()
-        self.check_button.grid(row=0,column=0)
-        self.volume_scale.bind('<ButtonRelease>',self.set_volume)
-        self.volume_scale.set(self.volume)
+        self.gui_button()
         if not self.last_played ==None:
             self.play(selected=self.last_played) 
         self.root.mainloop()
@@ -103,8 +58,52 @@ class music_player():
         except error:
             pass
         showinfo("Exit??","Closing The Player")
+    def gui_button(self):
+        self.load_img()
+        self.previous_button=Button(self.button_window,command=self.previous,image=self.image4)
+        self.previous_button.grid(row=0,column=1)
+        self.button=Button(self.button_window,command=self.play,image=self.image2)
+        self.button.grid(row=0,column=2)
+        self.volume_scale=Scale(self.button_window,orient='vertical',variable=self.volume,from_=100,to=0)
+        self.volume_scale.grid(row=0,column=5)
+        self.next_button=Button(self.button_window,command=self.next,image=self.image3)
+        self.next_button.grid(row=0,column=3)
+        self.check_button=Checkbutton(self.button_window,text="Say name",variable=self.speak_,onvalue=True,offvalue=False)
+        self.check_button.deselect()
+        self.check_button.grid(row=0,column=0)
+        self.volume_scale.bind('<ButtonRelease>',self.set_volume)
+        self.volume_scale.set(self.volume)
             
-            
+    def load_img(self):
+        self.image4=PhotoImage(file=join(getcwd(),"img/previous.png"))
+        self.image2=PhotoImage(file=join(getcwd(),"img/play.png"))
+        self.image3=PhotoImage(file=join(getcwd(),"img/next.png"))
+    def gui_data_init(self):
+        self.get_data()
+        if self.volume == None:
+            self.volume=25
+        if self.udplay_list==None or type(self.udplay_list) ==list:
+            self.udplay_list={}
+        def main_playlist():
+            self.list.delete(0,END)
+            self.get_data()
+        self.list_menu.add_command(label="Main",command=main_playlist) 
+        for pllist in self.udplay_list:
+            self.list_menu.add_command(label=pllist,command=lambda :self.play_play_list(pllist))
+
+    def gui_menu(self):
+        menu=Menu(self.root)
+        File=Menu(menu)
+        File.add_command(label="Add Folder ctrl+shift+o",command=self.open_folder)
+        File.add_command(label="Close Floder",command=self.close_floder)
+        playlist_menu=Menu(menu)
+        playlist_menu.add_command(label="CreatePlaylist",command=self.creat_playlist)
+        self.list_menu=Menu(menu)
+        playlist_menu.add_cascade(label="PLay Playlist",menu=self.list_menu)
+        menu.add_cascade(label="File",menu=File)
+        menu.add_cascade(label="Playlist",menu=playlist_menu)
+        self.root.config(menu=menu)
+               
                
     def open_folder(self,event=None,a=None,list_=None):
         file_name_list=[]
@@ -112,13 +111,14 @@ class music_player():
             list=self.list
         else:
             list=list_
-            # self.play_list=[]
         if a==None:
             a=askdirectory(initialdir="/")
+            if self.file_dir.__contains__(a):
+                showinfo("Folder Exict",f"The selected Folder {a} has been alredy added")
+                return None
             self.file_dir.append(a)
         for Directory_path,_,file_name in walk(a):
             file_name_list.append([Directory_path,file_name])
-            pass
         for dir_n,file_list in file_name_list:
             for name in file_list:
                 _,exe=splitext(name)
@@ -128,18 +128,18 @@ class music_player():
                     self.file_name_dir[name]=normcase(dir_n)
                     self.play_list.append(name)
             list.select_anchor(len(self.play_list))
-        
-            #print(self.list.size(),"size")
+
     def play(self,event=None,selected=None,repeat=False):
         
         if selected is None:
             index=self.list.curselection()
-            name=self.play_list[index[0]]
+            try:
+                name=self.play_list[index[0]]
+            except:
+                return None
             file=join(self.file_name_dir[name],name)
             self.list.selection_set(index)
-
         else:
-        
             file=selected
             _,name=split(file)
             index=self.play_list.index(name)
@@ -153,17 +153,13 @@ class music_player():
             mixer_music.pause()
             mixer_music.set_volume(self.volume)
             q=mixer_music.get_pos()
-            #print(q//1000)
             self.paused=True
             self.playing=False
-            # return None
-
         
         else:
             if len(file)>4:
                 self.image=PhotoImage(file=join(getcwd(),"img/pause.png"))
                 self.button.config(image=self.image)
-               
                 try:
                     if not self.paused or self.name != name:
                         if self.speak_.get():
@@ -173,22 +169,17 @@ class music_player():
                         mixer_music.play() 
                         if self.first_played !=None:
                             mixer_music.set_pos(self.pos)
-                            # self.pos=0
-                            #print("positions updated",mixer_music.get_pos())
                             self.first_played=None
                         mixer_music.play()
                         self.last_played=file
                         self.thread=Thread(target=self.check_end_position)
                         self.thread.start()
-                        
                         self.name=name
                         self.title_label["text"]=name
                     else:
-                        
                         mixer_music.unpause()
                         mixer_music.set_volume(self.volume)
                     self.playing=True
-                    # return None
                 except Exception as e:
                     Label(self.root,text=e).pack()
         if repeat:
@@ -197,10 +188,8 @@ class music_player():
         try:
             if name =="nt":
                 file=join(getcwd(),"Data\\music_player.json")
-                print(file)
             else:
                 file=join(getcwd(),"Data/music_player.json")
-                print(file)
             with open(normcase(file),"r") as f:
                 self.json_data=load(f)
                 self.file_dir=self.json_data["path"]
@@ -212,7 +201,6 @@ class music_player():
                 for file in self.file_dir:
                     self.open_folder(a=file)
         except Exception as e:
-            # if e is FileNotFoundError:
             if e is FileNotFoundError:
                 makedirs(join(getcwd(),"Data"))
             if name =="nt":
@@ -222,7 +210,6 @@ class music_player():
             with open(normcase(file),"w") as f:
                 f.write(r'{"path":[],"last_played":null,"pos":null,"volume":null,"play_list":{}}')
             self.get_data()
-            # elif JSONDecodeError:
 
     def set_volume(self,event):
         self.volume=self.volume_scale.get()
@@ -264,37 +251,26 @@ class music_player():
         self.list.select_clear(self.a[0])
         if self.a[0]+1==self.list.size():
             self.list.selection_set(0)
-            # self.list.selection_set(0)
             self.list.see(self.a[0]+1)
-            #print("Changed to 0 in try")    
         else:
             self.list.selection_set(self.a[0]+1)
             self.list.see(self.a[0]+1)
-            #print("tried Next")
-        
         self.play(repeat=True)
-        # self.list.
     def previous(self,event=None):
         self.a=self.list.curselection()
-        #print(self.a)
         self.list.select_clear(self.a[0])
         if self.a[0]==0:
             self.list.selection_set(self.list.size()-1)
-            # self.list.selection_set(0)
             self.list.see(self.list.size()-1)
-            # #print("Changed to 0 in try")    
         else:
             self.list.selection_set(self.a[0]-1)
             self.list.see(self.a[0]-1)
-            # #print("tried Next")
-        
         self.play(repeat=True)
 
     def check_end_position(self):
         audio=MP3(self.last_played)
         end=audio.info.length
         self.end_time=round(end/60,3)
-        #print(self.end_time)
         try:
             while not self.exited:
                 self.pos=mixer_music.get_pos()
@@ -305,25 +281,41 @@ class music_player():
                     self.progresslabel["text"] = f"{end_time}/{self.end_time}"
             return True
         except Exception as e:
-            #print(e)
             return False    
      
     def close_floder(self):
-        self.close_window=Tk()
+        def remove_folder(value):
+            print(self.file_dir.pop(self.radio_value.get()))
+            print(self.radio_value.get(),"Button")
+            print(value,"value")
+            self.list.delete(0,END)
+            for file in self.file_dir:
+                self.open_folder(a=file)
+
+            if not self.file_name_dir.__contains__(self.last_played):
+                try:
+                    mixer_music.stop()
+                except :
+                    pass
+                self.last_played =None
+            self.close_window.destroy()
+        self.close_window=Toplevel()
         self.radio_value=IntVar()
+        # self.radio_value.set("D")
         for vlue,file in enumerate(self.file_dir):
-            self.radio_button=Radiobutton(self.close_window,variable=self.radio_value,value=vlue,text=file).pack(anchor=W)
-        # self.radio_button.deselect()
-        Button(self.close_window,text="Remove",command=self.remove_folder).pack()
+            # print(file)
+            self.radio_button=Radiobutton(self.close_window,variable=self.radio_value,value=vlue,text=file)
+            self.radio_button.pack(anchor=W)
+            # print(self.radio_value.get())
+            # self.radio_value.set(vlue)
+            # print(vlue)
+        Button(self.close_window,text="Remove",command=lambda:remove_folder(self.radio_value.get())).pack()
         pass
-    def remove_folder(self):
-        #print("removing")
-        self.file_dir.pop(self.radio_value.get())
-        self.list.delete(0,END)
-        for file in self.file_dir:
-            self.open_folder(a=file)
-        self.close_window.destroy()
-        # self.close_folder()
+                # from tkinter import *
+        # %
+        # from tkinter import *
+        # root=Tk()
+        
     def play2(self,event=None):
         self.play()
         self.play()
@@ -340,11 +332,8 @@ class music_player():
                     return None
             udlist=[]
             for index in a:
-                    
-                # index=self.list.curselection()
                 name=self.play_list[index]
                 file=join(self.file_name_dir[name],name)
-                # self.list.selection_set(index)
                 udlist.append(file)
         
             self.udplay_list[play_name]=udlist
@@ -352,8 +341,6 @@ class music_player():
             showinfo("Created Playlist",f"Successfully Created Playlist {play_name}")
             self.play_list_window.destroy()
                 
-            pass
-
         def continuee():
             self.play_list_list=Listbox(self.play_list_window,width=30,selectmode=MULTIPLE)
             self.play_list_list.pack()
@@ -363,7 +350,7 @@ class music_player():
                 self.open_folder(a=file,list_=self.play_list_list)
             Button(self.play_list_window,text="Add to playlist",command=add).pack()
         
-        self.play_list_window=Tk()
+        self.play_list_window=Toplevel()
         Label(self.play_list_window,text="Enter Playlist name").pack()
         self.play_list_name=Entry(self.play_list_window)
         self.play_list_name.pack()
@@ -383,7 +370,6 @@ class music_player():
             self.play_list.append(bname)
 
         
-        pass
 
 music_player()
 
